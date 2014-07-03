6df3a3a0d4f461e519ed73a51e211297ec335116
drjones-shorty <pete.tamisin@gmail.com>
2014-07-03 18:01:59 -0400

    Changed sorting (issue #7) Added prioroty change (issue #6)

---
+++ js/db.js
@@ -0,0 +1,199 @@
+thoughtDeckStorage = {};
+thoughtDeckStorage.indexedDB = {};
+
+thoughtDeckStorage.indexedDB.db = null;
+
+thoughtDeckStorage.indexedDB.open = function() {
+    var version = 1;
+    var request = window.indexedDB.open("decks", version);
+
+    // We can only create Object stores in a versionchange transaction.
+    request.onupgradeneeded = function(e) {
+        var db = e.target.result;
+
+        // A versionchange transaction is started automatically.
+        e.target.transaction.onerror = thoughtDeckStorage.indexedDB.onerror;
+
+        if (db.objectStoreNames.contains("deck")) {
+            db.deleteObjectStore("deck");
+        }
+
+        var store = db.createObjectStore("deck", {
+            keyPath : "name"
+        });
+    };
+
+    request.onsuccess = function(e) {
+        thoughtDeckStorage.indexedDB.db = e.target.result;
+        thoughtDeckStorage.indexedDB.getAllDeckItems();
+    };
+
+    request.onerror = thoughtDeckStorage.indexedDB.onerror;
+};
+
+thoughtDeckStorage.indexedDB.addDeck = function(deckText, tags, desc, thoughts) {
+    var db = thoughtDeckStorage.indexedDB.db;
+    var trans = db.transaction(["deck"], "readwrite");
+    var store = trans.objectStore("deck");
+    var request = store.put({
+        "name" : deckText,
+        "tags" : tags,
+        "desc" : desc,
+        "thoughts" : thoughts,
+        "timeStamp" : new Date().getTime()
+    });
+
+    request.onsuccess = function(e) {
+        // Re-render all the deck's
+        thoughtDeckStorage.indexedDB.getAllDeckItems();
+    };
+
+    request.onerror = function(e) {
+        console.log(e.value);
+    };
+};
+
+thoughtDeckStorage.indexedDB.getAllDeckItems = function() {
+    $("#decklist").empty();
+
+    var db = thoughtDeckStorage.indexedDB.db;
+    var trans = db.transaction(["deck"], "readwrite");
+    var store = trans.objectStore("deck");
+
+    // Get everything in the store;
+    var keyRange = IDBKeyRange.lowerBound(0);
+    var cursorRequest = store.openCursor(keyRange);
+
+    cursorRequest.onsuccess = function(e) {
+        var result = e.target.result;
+        if (!!result == false)
+            return;
+
+        renderDeck(result.value);
+        result.continue();
+    };
+
+    cursorRequest.onerror = thoughtDeckStorage.indexedDB.onerror;
+};
+
+thoughtDeckStorage.indexedDB.getDeckItemsByName = function(name) {
+    clearDeck();
+    var db = thoughtDeckStorage.indexedDB.db;
+    var trans = db.transaction(["deck"], "readwrite");
+    var store = trans.objectStore("deck");
+    var request = store.get(name);
+    request.onerror = function(event) {
+        // Handle errors!
+    };
+    request.onsuccess = function(event) {
+        // Do something with the request.result!
+        var deck = document.getElementById('deckname');
+        var tags = document.getElementById('tags');
+        var desc = document.getElementById('desc');
+
+        var thoughts = [];
+        deck.innerHTML = request.result.name;
+        tags.value = request.result.tags;
+        desc.value = request.result.desc;
+        thoughts = JSON.parse(request.result.thoughts);
+
+        console.log('retrived thoughts:' + request.result.thoughts);
+
+        thoughts.forEach(function(entry) {
+            console.log('thought:' + entry.thoughtText);
+            $('#sortable').append('<li id="' + entry.thoughtId + '" class="' + entry.liClasses + '"><div id="thought' + $cnt + '" class="' + entry.thoughtClasses + '">' + entry.thoughtText + '</div></li>');
+            $(".thought-default").more({
+                length : 80,
+                moreText : '<span style="text-shadow:none;color:gray;">more</span>',
+                lessText : '<span style="text-shadow:none;color:gray;">less</span>'
+            });
+
+        });
+        $.mobile.navigate("#thought-edit", {
+            transition : "slide",
+            info : "info about the #bar hash"
+        });
+    };
+};
+
+function renderDeck(row) {
+    var decks = document.getElementById("decklist");
+    var dv = document.createElement("div");
+    var a = document.createElement("a");
+    var a1 = document.createElement("a");
+
+    var t = document.createTextNode("");
+    t.data = row.name;
+    console.log(row.thoughts);
+
+    //    a.addEventListener("click", function(e) {
+    //        thoughtDeckStorage.indexedDB.deleteDeck(row.name);
+    //    });
+
+    a1.addEventListener("click", function(e) {
+        thoughtDeckStorage.indexedDB.getDeckItemsByName(row.name);
+    });
+
+    a1.textContent = t.data;
+    dv.appendChild(a1);
+    dv.classList.add("deckimage");
+
+    $("#decklist").owlCarousel({
+        itemsCustom : [[0, 2], [450, 4], [600, 7], [700, 9], [1000, 10], [1200, 12], [1400, 13], [1600, 15]],
+        navigation : false
+    });
+    $("#decklist").data('owlCarousel').addItem(dv);
+
+}
+
+thoughtDeckStorage.indexedDB.deleteDeck = function(id) {
+    var db = thoughtDeckStorage.indexedDB.db;
+    var trans = db.transaction(["deck"], "readwrite");
+    var store = trans.objectStore("deck");
+
+    var request = store.delete(id);
+
+    request.onsuccess = function(e) {
+        thoughtDeckStorage.indexedDB.getAllDeckItems();
+    };
+
+    request.onerror = function(e) {
+        console.log("Error Adding: ", e);
+    };
+};
+
+function init() {
+    thoughtDeckStorage.indexedDB.open();
+    // open displays the data previously saved
+}
+
+window.addEventListener("DOMContentLoaded", init, false);
+
+function addDeck() {
+    var deck = document.getElementById('deckname');
+    var tags = document.getElementById('tags');
+    var desc = document.getElementById('desc');
+
+    if (deck === null) {
+        alert("Specify Deck Name");
+        return;
+    }
+    var arThoughts = [];
+    $(".thought-default").each(function(index) {
+        var obThought = {};
+
+        obThought.thoughtId = $(this).attr('id');
+        obThought.thoughtText = $(this).html();
+        obThought.thoughtClasses = $(this).attr('class');
+        obThought.liClasses = $(this).parent().attr('class');
+
+        console.log('Adding:' + JSON.stringify(obThought));
+
+        arThoughts.push(obThought);
+
+    });
+
+    thoughtDeckStorage.indexedDB.addDeck(deck.innerHTML, tags.value, desc.value, JSON.stringify(arThoughts));
+
+    clearDeck();
+}
--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
-/* Author:
 
- */
-
-$(document).bind("mobileinit", function () {
-    $.event.special.tap.tapholdThreshold = 1000,
-    $.event.special.swipe.durationThreshold = 999;
+$(document).bind("mobileinit", function() {
+    $.event.special.tap.tapholdThreshold = 750;
     $.event.special.tap.emitTapOnTaphold = false;
 });
-    
+
 $("select:has(option[value=]:first-child)").on('change', function() {
     $(this).toggleClass("empty", $.inArray($(this).val(), ['', null]) >= 0);
 }).trigger('change');
@@ -29,269 +25,74 @@ $(document).ready($(function() {
 }));
 
 
-    
 $(function() {
-    $("#sortable").sortable({
-        placeholder : "ui-state-highlight"
-    });
+    $("#sortable").sortable();
+    $("#sortable").sortable('disable');
     $("#sortable").disableSelection();
 
-    $("#sortable").on("taphold", function() {
+/*
+    $(".thought-default").on("click", function() {
         $('#thought_id').val($(this).attr("id"));
-        $('#thought').val($(this).HTML);
+        $('#thought').val($(this).attr("id"));
         $("#fireThoughtBubble").click();
-        //  $.mobile.navigate( "#thoughtBubble", { info: "info about the #bar hash" });
     });
-    
+*/ 
+
+/*
+    $( "#sortable" ).bind( "sortstop", function(event, ui) {    
+        if (Math.abs(ui.offset.top - ui.originalPosition.top)>35){
+            $(this).sortable();
+        }
+    });
+*/   
+
+  $( "#sortable" ).on( "taphold", tapholdHandler );
+ 
+  // Callback function references the event target and adds the 'swiperight' class to it
+  function tapholdHandler( event ){
+    $('#sortable').sortable('enable');
+    $( event.target ).parent().addClass( "glow" );
+  }
+
+
+  $( "#sortable" ).bind( "sortstop", function(event, ui) {    
+    $('#sortable').sortable('disable');
+    //alert(ui.item[0].attr("class"));
+    $('.li-default').removeClass( "glow" );
+  });
+
+    // Bind the swiperightHandler callback function to the swipe event on div.box
+  $( "#sortable" ).on( "swiperight", swiperightHandler );
+ 
+  // Callback function references the event target and adds the 'swiperight' class to it
+  function swiperightHandler( event, ui ){
+    if($( event.target ).parent().hasClass( "maj_imp" )) {
+        $( event.target ).parent().removeClass( "maj_imp" );
+        $( event.target ).parent().addClass( "reg_imp" );        
+    } else if($( event.target ).parent().hasClass( "reg_imp" )) {
+        $( event.target ).parent().removeClass( "reg_imp" );
+        $( event.target ).parent().addClass( "min_imp" );        
+        
+    }
+  }
+
+    // Bind the swiperightHandler callback function to the swipe event on div.box
+  $( "#sortable" ).on( "swipeleft", swipeleftHandler );
+ 
+  // Callback function references the event target and adds the 'swiperight' class to it
+  function swipeleftHandler( event, ui ){
+    if($( event.target ).parent().hasClass( "min_imp" )) {
+        $( event.target ).parent().removeClass("min_imp");
+        $( event.target ).parent().addClass( "reg_imp" );        
+    } else if($( event.target ).parent().hasClass("reg_imp")) {
+        $( event.target ).parent().removeClass( "reg_imp" );
+        $( event.target ).parent().addClass( "maj_imp" );        
+    }
+  }
+
+
 });
 
-$(document).ready($(function() {
-    $('.cancelMarkupButton').on('click', function() {
-        $("#newMarkupContainer").hide();
-    });
-}));
-
-thoughtDeckStorage = {};
-thoughtDeckStorage.indexedDB = {};
-
-thoughtDeckStorage.indexedDB.db = null;
-
-thoughtDeckStorage.indexedDB.open = function() {
-    var version = 1;
-    var request = window.indexedDB.open("decks", version);
-
-    // We can only create Object stores in a versionchange transaction.
-    request.onupgradeneeded = function(e) {
-        var db = e.target.result;
-
-        // A versionchange transaction is started automatically.
-        e.target.transaction.onerror = thoughtDeckStorage.indexedDB.onerror;
-
-        if (db.objectStoreNames.contains("deck")) {
-            db.deleteObjectStore("deck");
-        }
-
-        var store = db.createObjectStore("deck", {
-            keyPath : "name"
-        });
-    };
-
-    request.onsuccess = function(e) {
-        thoughtDeckStorage.indexedDB.db = e.target.result;
-        thoughtDeckStorage.indexedDB.getAllDeckItems();
-    };
-
-    request.onerror = thoughtDeckStorage.indexedDB.onerror;
-};
-
-thoughtDeckStorage.indexedDB.addDeck = function(deckText, tags, desc, thoughts) {
-    var db = thoughtDeckStorage.indexedDB.db;
-    var trans = db.transaction(["deck"], "readwrite");
-    var store = trans.objectStore("deck");
-    var request = store.put({
-        "name" : deckText,
-        "tags" : tags,
-        "desc" : desc,
-        "thoughts" : thoughts,
-        "timeStamp" : new Date().getTime()
-    });
-
-    request.onsuccess = function(e) {
-        // Re-render all the deck's
-        thoughtDeckStorage.indexedDB.getAllDeckItems();
-    };
-
-    request.onerror = function(e) {
-        console.log(e.value);
-    };
-};
-
-thoughtDeckStorage.indexedDB.getAllDeckItems = function() {
-    $("#decklist").empty();
-
-    var db = thoughtDeckStorage.indexedDB.db;
-    var trans = db.transaction(["deck"], "readwrite");
-    var store = trans.objectStore("deck");
-
-    // Get everything in the store;
-    var keyRange = IDBKeyRange.lowerBound(0);
-    var cursorRequest = store.openCursor(keyRange);
-
-    cursorRequest.onsuccess = function(e) {
-        var result = e.target.result;
-        if (!!result == false)
-            return;
-
-        renderDeck(result.value);
-        result.
-        continue();
-    };
-
-    cursorRequest.onerror = thoughtDeckStorage.indexedDB.onerror;
-};
-
-thoughtDeckStorage.indexedDB.getDeckItemsByName = function(name) {
-    clearDeck();
-    var db = thoughtDeckStorage.indexedDB.db;
-    var trans = db.transaction(["deck"], "readwrite");
-    var store = trans.objectStore("deck");
-    var request = store.get(name);
-    request.onerror = function(event) {
-        // Handle errors!
-    };
-    request.onsuccess = function(event) {
-        // Do something with the request.result!
-        var deck = document.getElementById('deckname');
-        var tags = document.getElementById('tags');
-        var desc = document.getElementById('desc');
-
-        var thoughts = [];
-        deck.innerHTML = request.result.name;
-        tags.value = request.result.tags;
-        desc.value = request.result.desc;
-        thoughts = JSON.parse(request.result.thoughts);
-
-        console.log('retrived thoughts:' + request.result.thoughts);
-
-        thoughts.forEach(function(entry) {
-            console.log('thought:' + entry.thoughtText);
-            $('#sortable').append('<li id="' + entry.thoughtId + '" class="' + entry.liClasses + '"><div id="thought' + $cnt + '" class="' + entry.thoughtClasses + '">' + entry.thoughtText + '</div></li>');
-            $(".thought-default").more({
-                length : 80,
-                moreText : '<span style="text-shadow:none;color:gray;">more</span>',
-                lessText : '<span style="text-shadow:none;color:gray;">less</span>'
-            });
-
-        });
-        $.mobile.navigate("#thought-edit", {
-            transition : "slide",
-            info : "info about the #bar hash"
-        });
-    };
-};
-
-function renderDeck(row) {
-    var decks = document.getElementById("decklist");
-    var dv = document.createElement("div");
-    var a = document.createElement("a");
-    var a1 = document.createElement("a");
-
-    var t = document.createTextNode("");
-    t.data = row.name;
-    console.log(row.thoughts);
-
-    //    a.addEventListener("click", function(e) {
-    //        thoughtDeckStorage.indexedDB.deleteDeck(row.name);
-    //    });
-
-    a1.addEventListener("click", function(e) {
-        thoughtDeckStorage.indexedDB.getDeckItemsByName(row.name);
-    });
-
-    a1.textContent = t.data;
-    dv.appendChild(a1);
-    dv.classList.add("deckimage");
-
-    $("#decklist").owlCarousel({
-        itemsCustom : [[0, 2], [450, 4], [600, 7], [700, 9], [1000, 10], [1200, 12], [1400, 13], [1600, 15]],
-        navigation : false
-    });
-    $("#decklist").data('owlCarousel').addItem(dv);
-
-}
-
-thoughtDeckStorage.indexedDB.deleteDeck = function(id) {
-    var db = thoughtDeckStorage.indexedDB.db;
-    var trans = db.transaction(["deck"], "readwrite");
-    var store = trans.objectStore("deck");
-
-    var request = store.
-    delete (id);
-
-    request.onsuccess = function(e) {
-        thoughtDeckStorage.indexedDB.getAllDeckItems();
-    };
-
-    request.onerror = function(e) {
-        console.log("Error Adding: ", e);
-    };
-};
-
-function init() {
-    thoughtDeckStorage.indexedDB.open();
-    // open displays the data previously saved
-}
-
-window.addEventListener("DOMContentLoaded", init, false);
-
-function addDeck() {
-    var deck = document.getElementById('deckname');
-    var tags = document.getElementById('tags');
-    var desc = document.getElementById('desc');
-
-    if (deck === null) {
-        alert("Specify Deck Name");
-        return;
-    }
-    var arThoughts = [];
-    $(".thought-default").each(function(index) {
-        var obThought = {};
-
-        obThought.thoughtId = $(this).attr('id');
-        obThought.thoughtText = $(this).html();
-        obThought.thoughtClasses = $(this).attr('class');
-        obThought.liClasses = $(this).parent().attr('class');
-
-        console.log('Adding:' + JSON.stringify(obThought));
-
-        arThoughts.push(obThought);
-
-    });
-
-    thoughtDeckStorage.indexedDB.addDeck(deck.innerHTML, tags.value, desc.value, JSON.stringify(arThoughts));
-
-    clearDeck();
-}
-
-
-function setupAutoComplete() {
-
-    //Create the autocomplete
-    $("#searchDecks").autocomplete({
-        source: function( request, response ) {
-
-            console.log("Going to look for "+ request.term);
-
-            var transaction = thoughtDeckStorage.indexedDB.db.transaction(["deck"], "readonly");
-            var result = [];
-
-            transaction.oncomplete = function(event) {
-                response(result);
-            }
-
-            //TODO: Handle the error and return to it jQuery UI
-            var objectStore = transaction.objectStore("deck");
-
-            //Credit: http://stackoverflow.com/a/8961462/52160
-            var range = IDBKeyRange.bound(request.term.toLowerCase(), request.term.toLowerCase() + "z");
-            var index = objectStore.index("searchkey");
-
-            index.openCursor(range).onsuccess = function(event) {
-                var cursor = event.target.result;
-                if(cursor) {
-                    result.push({value: cursor.value.lastname + ", " + cursor.value.firstname,person: cursor.value});
-                    cursor.continue();
-                }
-            };
-
-        },
-        minLength:2,
-        select:function(event,ui) {
-            $("#displayEmployee").show().html(template(ui.item.person));
-        }
-    });
-
-}
 
 
 function clearDeck() {
